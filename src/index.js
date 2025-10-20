968ccea3a8472e406a4cff30e49fb1c2498a5972
suvijohanna <suvi.mynttinen@metropolia.fi>
2025-10-20 15:27:56 +0300

    implement basic HTTP server with GET endpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+import http from "http";
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(`HTTP Request: ${req.method} ${req.url}`);
+
+  if (req.method === "GET" && req.url === "/kukkuu") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end('{"message": "no kukkuu!"}');
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not found :(");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
