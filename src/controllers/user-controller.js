29fbef3ac073ae7eede3fe503ba1383e560fc042
suvijohanna <suvi.mynttinen@metropolia.fi>
2025-11-25 14:04:32 +0200

    feat: add user authentication with bcrypt and API documentation

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,11 +2,13 @@ import jwt from 'jsonwebtoken';
 import {addUser, selectUserByUsername} from '../models/user-model.js';
 import 'dotenv/config';
 import {validationResult} from 'express-validator';
+import bcrypt from 'bcryptjs';
 
 const postLogin = async (req, res) => {
   console.log('postLogin', req.body);
   const user = await selectUserByUsername(req.body.username);
-  const passwordMatch = user && user.password === req.body.password;
+  const passwordMatch =
+    user && (await bcrypt.compare(req.body.password, user.password));
   if (passwordMatch) {
     const token = jwt.sign(user, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN,
@@ -17,18 +19,23 @@ const postLogin = async (req, res) => {
   }
 };
 
-const postUser = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+const postUser = async (req, res, next) => {
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json(errors.array());
+  // }
   const newUser = req.body;
-  newUser.user_level_id = 1; // default user level
+  // default user level
+  newUser.user_level_id = 1;
+  // replace plain text password with hashed password
+  newUser.password = await bcrypt.hash(newUser.password, 10);
   const result = await addUser(newUser);
   if (result.user_id) {
     res.json({message: 'User created', user_id: result.user_id});
   } else {
-    return res.status(400).res.json({});
+    const error = new Error(result.error);
+    error.status = 400;
+    next(error);
   }
 };
 
--- src/router/auth-router.js
+++ src/router/auth-router.js
@@ -4,7 +4,106 @@ import {authenticateToken} from '../../middlewares/authentication.js';
 
 const authRouter = express.Router();
 
-authRouter.route('/login').post(postLogin);
-authRouter.route('/me').get(authenticateToken, getMe);
+/**
+ * @apiDefine all No authentication needed.
+ */
+
+/**
+ * @apiDefine token Logged in user access only
+ * Valid authentication token must be provided within request.
+ */
+
+/**
+ * @apiDefine UnauthorizedError
+ * @apiError UnauthorizedError User name or password invalid.
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 403 Forbidden
+ *     {
+ *       "error": {
+ *         "message": "username/password invalid",
+ *         "status": 401
+ *       }
+ *     }
+ */
+
+authRouter
+  .route('/login')
+
+  /**
+   * @api {post} /auth/login Login
+   * @apiVersion 1.0.0
+   * @apiName PostLogin
+   * @apiGroup Authentication
+   * @apiPermission all
+   *
+   * @apiDescription Sign in and get an authentication token for the user.
+   *
+   * @apiParam {String} username Username of the user.
+   * @apiParam {String} password Password of the user.
+   *
+   * @apiParamExample {json} Request-Example:
+   *    {
+   *      "username": "johnd",
+   *      "password": "examplepass"
+   *    }
+   *
+   * @apiSuccess {String} token Token for the user authentication.
+   * @apiSuccess {Number} user_id User ID.
+   * @apiSuccess {String} user_name Username of the user.
+   * @apiSuccess {String} email Email address of the user.
+   * @apiSuccess {Number} user_level_id User level identifier.
+   * @apiSuccess {String} created_at Registration timestamp of user.
+   *
+   * @apiSuccessExample Success-Response:
+   *    HTTP/1.1 200 OK
+   *    {
+   *      "user_id": 21,
+   *      "username": "johnd",
+   *      "email": "johnd@example.com",
+   *      "user_level_id": 2,
+   *      "created_at": "2023-11-28T12:30:21.000Z",
+   *      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+   *    }
+   *
+   * @apiUse UnauthorizedError
+   */
+  .post(postLogin);
+
+authRouter
+  .route('/me')
+  /**
+   * @api {get} /auth/me Request information about current user
+   * @apiVersion 1.0.0
+   * @apiName GetMe
+   * @apiGroup Authentication
+   * @apiPermission token
+   * @apiHeader {String} Authorization Bearer token.
+   *
+   * @apiSuccess {Object} user User info.
+   * @apiSuccess {Number} user.user_id Id of the User.
+   * @apiSuccess {String} user.username Username of the User.
+   * @apiSuccess {String} user.email email of the User.
+   * @apiSuccess {Number} user.user_level_id User level id of the User.
+   * @apiSuccess {Number} user.iat Token creation timestamp.
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "user_id": 21,
+   *       "username": "johnd",
+   *       "email": "johnd@example.com",
+   *       "user_level_id": 2,
+   *       "iat": 1701279021
+   *     }
+   *
+   * @apiError InvalidToken Authentication token was invalid.
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 403 Forbidden
+   *     {
+   *       "message": "invalid token"
+   *     }
+   */
+  .get(authenticateToken, getMe);
 
 export default authRouter;
